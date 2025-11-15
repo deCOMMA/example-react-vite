998bb6b56f2a18e6b1298405c707fc0c76d8f712
Dmitry <sherbinovscky2018@yandex.ru>
2025-11-15 19:40:14 +0300

    ERV25: исправление ошибок CI

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import analyzer from 'vite-bundle-analyzer';
 // https://vite.dev/config/
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
-const dirname =
-    typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+//import { fileURLToPath } from 'node:url';
+// import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+// import { playwright } from '@vitest/browser-playwright';
+// const dirname =
+//     typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
