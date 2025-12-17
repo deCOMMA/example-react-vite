cbd3e952e16fd8dfdbb65d417c3dd1d04446dbd7
Dmitry <sherbinovscky2018@yandex.ru>
2025-12-17 18:05:12 +0300

    ERV51: add DynamicModuleFolder and its integration with LoginForm

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.tsx
@@ -3,9 +3,9 @@ import cls from './LoginForm.module.css';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/shared/ui/Button/Button';
 import { Input } from '@/shared/ui/Input/Input';
-import { useSelector, useStore } from 'react-redux';
-import { loginActions, loginReducer, } from '../../model/slice/loginSlice';
-import { memo, useCallback, useEffect, } from 'react';
+import { useSelector } from 'react-redux';
+import { loginActions, loginReducer } from '../../model/slice/loginSlice';
+import { memo, useCallback } from 'react';
 import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
 import { useAppDispatch } from '@/app/providers/Store/config/hooks';
 import { Loader } from '@/shared/ui/Loader/Loader';
@@ -14,31 +14,24 @@ import { getLoginUsername } from '../../model/selectors/getLoginUsername/getLogi
 import { getLoginPassword } from '../../model/selectors/getLoginPassword/getLoginPassword';
 import { getLoginLoading } from '../../model/selectors/getLoginLoading/getLoginLoading';
 import { getLoginError } from '../../model/selectors/getLoginError/getLoginError';
-import type { ReduxStoreWitnManager } from '@/app/providers/Store';
+import { DynamicModuleFolder, type ReducerList } from '@/shared/helpers/components/DynamicModuleFolder/DynamicModuleFolder';
 
 export type LoginFormProps = {
     className?: string;
 };
 
+const initialReducers: ReducerList = {
+    loginForm: loginReducer
+}
+
 const LoginForm = memo(({ className, ...otherProps }: LoginFormProps) => {
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
-    const store = useStore() as ReduxStoreWitnManager;
 
     const username = useSelector(getLoginUsername)
     const password = useSelector(getLoginPassword)
     const isLoading = useSelector(getLoginLoading)
     const error = useSelector(getLoginError)
-
-    useEffect(() => {
-        store.reducerManager.add('loginForm', loginReducer)
-
-        return () => {
-            store.reducerManager.remove('loginForm');
-        }
-        //elint-disable-next-line
-    }, [])
-
 
     const onChangeUsername = useCallback(
         (value: string) => {
@@ -61,33 +54,35 @@ const LoginForm = memo(({ className, ...otherProps }: LoginFormProps) => {
     const classNames = clsx(cls.LoginForm, className);
 
     return (
-        <div className={classNames} {...otherProps}>
-            <Text title={t('Форма авторизации')} />
-            {error && <Text text={error} thema='error' />}
-            <Input
-                value={username}
-                className={cls.input}
-                placeholder={t('Введите username')}
-                autoFocus
-                onChange={onChangeUsername}
-            ></Input>
-            <Input
-                value={password}
-                className={cls.input}
-                placeholder={t('Введите password')}
-                onChange={onChangePassword}
-            ></Input>
-            <Button
-                theme='clearInv'
-                className={cls.loginBtn}
-                style={{ marginTop: '40px' }}
-                onClick={onLoginClick}
-                disabled={isLoading}
-            >
-                {t('Войти ')}
-            </Button>
-            {isLoading && <Loader />}
-        </div>
+        <DynamicModuleFolder reducers={initialReducers} removeAfterUnmount={true}>
+            <div className={classNames} {...otherProps}>
+                <Text title={t('Форма авторизации')} />
+                {error && <Text text={error} thema='error' />}
+                <Input
+                    value={username}
+                    className={cls.input}
+                    placeholder={t('Введите username')}
+                    autoFocus
+                    onChange={onChangeUsername}
+                ></Input>
+                <Input
+                    value={password}
+                    className={cls.input}
+                    placeholder={t('Введите password')}
+                    onChange={onChangePassword}
+                ></Input>
+                <Button
+                    theme='clearInv'
+                    className={cls.loginBtn}
+                    style={{ marginTop: '40px' }}
+                    onClick={onLoginClick}
+                    disabled={isLoading}
+                >
+                    {t('Войти ')}
+                </Button>
+                {isLoading && <Loader />}
+            </div>
+        </DynamicModuleFolder>
     );
 });
 
---
+++ src/shared/helpers/components/DynamicModuleFolder/DynamicModuleFolder.tsx
@@ -0,0 +1,51 @@
+import type { ReduxStoreWitnManager } from "@/app/providers/Store";
+import { useAppDispatch } from "@/app/providers/Store/config/hooks";
+import type { StateSchemaKey } from "@/app/providers/Store/config/StateShema";
+import type { Reducer } from "@reduxjs/toolkit";
+import { useEffect } from "react";
+import { useStore } from "react-redux";
+
+export type ReducerList = {
+    [name in StateSchemaKey]?: Reducer;
+}
+
+type DynamicModuleFolderProps = {
+    children?: React.ReactNode;
+    reducers: ReducerList;
+    removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleFolder = (props: DynamicModuleFolderProps) => {
+
+    const {
+        children,
+        reducers,
+        removeAfterUnmount
+    } = props;
+
+    const store = useStore() as ReduxStoreWitnManager;
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        Object.entries(reducers).forEach(([name, reducer]) => {
+            store.reducerManager.add(name as StateSchemaKey, reducer)
+            dispatch({ type: `@INIT ${name} reducer` })
+        })
+
+        return () => {
+            if (removeAfterUnmount) {
+                Object.entries(reducers).forEach(([name]) => {
+                    store.reducerManager.remove(name as StateSchemaKey);
+                    dispatch({ type: `@DESTROY ${name} reducer` })
+                })
+            }
+        }
+        //elint-disable-next-line
+    }, [])
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
